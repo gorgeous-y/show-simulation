1a08c76cb95c76024c91306aa9c1952d305a889f
guoyu <guoyu@aerozhonghuan.com>
2021-08-20 16:53:38 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 表单
 import './plugins/element.js'
+// 全局
+import './assets/css/global.css'
+// 字体
+import './assets/fonts/iconfont.css'
 
+import axios from 'axios'
+
+// 配置请求的根路径
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http=axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {Button,Form,FormItem,Input,Message} from 'element-ui'
 
+// 全局导入 import 'element-ui/lib/theme-chalk/index.css'(报错问题);
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// Message需要全局挂载 和以上不一样，$message是自定义属性 只要合法即可
+Vue.prototype.$message = Message
+// 不能连续引入 Vue.use(Button,Form,FormItem,Input)写法错误
